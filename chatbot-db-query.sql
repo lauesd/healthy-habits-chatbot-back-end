c11acc07ad4cba69a6434aa9dbcd6a3646fa6bf8
Gabriel Organista <organistamunozjose@gmail.com>
2026-02-09 16:33:10 -0500

    Im improving db

--- chatbot-db-query.sql
+++ chatbot-db-query.sql
@@ -1,4 +1,12 @@
-create db 
+drop database if exists habit_tracker;
+create database habit_tracker;
+
+CREATE TABLE user_profile (
+    id INT PRIMARY KEY CHECK (id = 1),
+    username VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE user_events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
